6721fa4cef8372b3e99bced4331e0234b4dd6e4a
Samuel Vasko <samvasko@gmail.com>
2016-03-30 11:33:52 +0200

    Added go test runner

---
+++ tester/tester.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+import "os"
+import "log"
+import "bufio"
+import "github.com/jacobsa/go-serial/serial"
+import "github.com/jessevdk/go-flags"
+
+func main() {
+
+	var opts struct {
+		BaudRate uint   `short:"b" long:"baud" default:"9600" description:"Baud rate for transimission"`
+		PortName string `short:"p" long:"port" required:"true" description:"Serial port to read"`
+	}
+
+	_, err := flags.Parse(&opts)
+
+	if err != nil {
+		os.Exit(0)
+		log.Fatal(err)
+	}
+
+	options := serial.OpenOptions{
+		PortName:        opts.PortName,
+		BaudRate:        opts.BaudRate,
+		DataBits:        8,
+		StopBits:        1,
+		MinimumReadSize: 4,
+	}
+
+	// Open the port.
+	port, err := serial.Open(options)
+	if err != nil {
+		log.Fatalf("serial.Open: %v", err)
+	}
+
+	// Make sure to close it later.
+	defer port.Close()
+
+	scanner := bufio.NewScanner(port)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
